89e244029e1e71a0532e8d316c67f6bfc8a28eaf
Klim Tsoutsman <klim@tsoutsman.com>
2022-07-23 15:42:37 +1000

    Add `sys::theseus`
    Depends on #1 and theseus-os/Theseus#575.
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- library/std/src/sys/mod.rs
+++ library/std/src/sys/mod.rs
@@ -37,6 +37,9 @@ cfg_if::cfg_if! {
     } else if #[cfg(target_os = "hermit")] {
         mod hermit;
         pub use self::hermit::*;
+    } else if #[cfg(target_os = "theseus")] {
+        mod theseus;
+        pub use self::theseus::*;
     } else if #[cfg(target_os = "wasi")] {
         mod wasi;
         pub use self::wasi::*;
