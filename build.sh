1917bb255e06cdae344a5d94eeef56dce4b19aaa
Klim Tsoutsman <klim@tsoutsman.com>
2022-08-08 20:04:23 +1000

    Fix build scripts
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-RUSTFLAGS="-L $PWD/build/x86_64-unknown-linux-gnu/stage0-std/x86_64-theseus/release/deps" ./x.py build library/std --stage 0 --target ./x86_64-theseus.json 
+RUSTFLAGS="--emit=obj -C code-model=large -C relocation-model=static -L $PWD/build/x86_64-unknown-linux-gnu/stage0-std/x86_64-theseus/release/deps" ./x.py build library/std --stage 0 --target ./x86_64-theseus.json 
--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,4 @@
+set -e
 ./x.py clean
-./x.py build library/alloc --stage 0 --target ./x86_64-theseus.json
-RUSTFLAGS="-L $PWD/build/x86_64-unknown-linux-gnu/stage0-std/x86_64-theseus/release/deps" ./x.py build library/std --stage 0 --target ./x86_64-theseus.json 
+RUSTFLAGS="--emit=obj -C code-model=large -C relocation-model=static" ./x.py build library/alloc --stage 0 --target ./x86_64-theseus.json
+./build.sh
